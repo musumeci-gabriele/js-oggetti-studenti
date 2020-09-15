ad13c6f04ee6e646e3db32fe68e5b8ca76e82110
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-15 17:53:32 +0200

    parte 3

--- js/script.js
+++ js/script.js
@@ -4,16 +4,18 @@
 
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 $(document).ready(function(){
+
 // esercizio uno
   var student = {
     "nome" : "gabriele",
     "cognome" : "musumeci",
     "eta" : "37"
   }
-  // ciclo della variabile key in array
+  // ciclo della variabile key in array per visualizzare i dati studente
   for (var key in student){
     console.log(student[key]);
   }
+  console.log("-------fine esercizio---------");
 // esercizio due
  var students = [
    {
@@ -36,9 +38,26 @@ $(document).ready(function(){
      var studenti = students[i];
      console.log(studenti[key]);
    }
-   
- }
 
+ }
+ console.log("-------fine esercizio---------");
+
+// esercizio 3
+  var datiStud = ["Nome","Cognome","Eta"];
+  var classe = [
+    {
+      "nome" : "",
+      "cognome" : "",
+      "eta" : "",
+    }
+  ];
+
+  for (var key in classe, i = 0){
+    var stud = prompt("Insersci "+ datiStud[i] + "del nuovo studente :");
+    $(classe[key]).push(stud);
+    i++;
+    console.log(classe[key]);
+  }
 
 
 
